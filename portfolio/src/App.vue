5707fc823dd8cc6e338c3a7cbc7502164548ba56
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2024-11-25 17:19:56 +0530

    initial commit(complete header)

---
+++ portfolio/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <HeaderCom />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HeaderCom from "./components/HeaderCom.vue";
+export default {
+  name: "App",
+
+  components: {
+    HeaderCom,
+  },
+};
+</script>
---
+++ portfolio/src/components/HeaderCom.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app-bar
+    style="padding: 0px 90px"
+    app
+    color="white"
+    light
+    elevation="6"
+    elevate-on-scroll
+  >
+    <v-toolbar-title class="font-weight-bold">My Portfolio.</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-list class="d-flex align-center">
+      <v-list-item v-for="(menu, index) in menus" :key="index" :to="menu.route">
+        <v-list-item-title>{{ menu.title }} </v-list-item-title>
+      </v-list-item>
+      <v-btn class="ml-7" text>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-btn outlined color="success" class="ml-6">Get Started</v-btn>
+    </v-list>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "HeaderCom",
+
+  data: () => ({
+    menus: [
+      { title: "Home", route: "home" },
+      { title: "About us", route: "about-us" },
+      { title: "Portfolio", route: "protfolio" },
+      { title: "Project", route: "project" },
+      { title: "Gallery", route: "galley" },
+      { title: "Contact", route: "contact" },
+    ],
+  }),
+};
+</script>
